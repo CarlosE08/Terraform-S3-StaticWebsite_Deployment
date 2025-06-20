9b48638ac4905b65ed269913b43d0114ad74fe83
CarlosE08 <carlos.escobar6409@alumnos.udg.mx>
2025-06-20 12:12:14 -0600

    fix: Remove unused output for S3 bucket website endpoint

--- modules/S3/outputs.tf
+++ modules/S3/outputs.tf
@@ -8,11 +8,6 @@ output "bucket_arn" {
   value       = aws_s3_bucket.website.arn
 }
 
-output "bucket_website_endpoint" {
-  description = "Endpoint del sitio web estático en S3"
-  value       = aws_s3_bucket.website.website_endpoint
-}
-
 output "bucket_id" {
   description = "ID del bucket S3"
   value       = aws_s3_bucket.website.id
--- outputs.tf
+++ outputs.tf
@@ -12,8 +12,3 @@ output "bucket_id" {
   description = "ID del bucket S3"
   value       = module.aws_s3.bucket_id # Accede al output del módulo aws_s3
 }
-
-output "s3_bucket_website_endpoint" {
-  description = "sitio web estático en S3"
-  value       = module.aws_s3.bucket_website_endpoint # Accede al output del módulo aws_s3
-}
